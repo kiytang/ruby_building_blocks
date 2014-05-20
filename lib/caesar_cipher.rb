98285de31a78c3968b2eb12556757b1c1b2ca36f
Kiy Hong Tang <kiyhong@gmail.com>
2014-05-20 16:37:03 +0100

    Added decode_text method

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -12,5 +12,19 @@ def encode_text(text, shift_value)
   encoded_text
 end
 
+def decode_text(text, shift_value)
+    letters = ('a'..'z').to_a.concat(('A'..'Z').to_a)
+    decoded_text = ""
+    
+    text.each_char do |char|
+    if letters.include?(char)
+      decoded_text += letters[letters.index(char) - shift_value]
+    else
+      decoded_text += char  
+    end
+  end
+  decoded_text
+end
 
 p encode_text("Ciao Mondo!", 5)
+p decode_text("Hnft Rtsit!", 5)
